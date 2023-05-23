3666273cc7499ee0ce13543f8845a1e20669b1d0
Aleksey <mc_gra_dy@mail.ru>
2023-05-23 10:28:32 +0300

    Initial commit

---
+++ connector.py
@@ -0,0 +1,70 @@
+import json
+import os
+
+
+class Connector:
+    """
+    Класс коннектор к файлу, обязательно файл должен быть в json формате
+    не забывать проверять целостность данных, что файл с данными не подвергся
+    внешнего деградации
+    """
+    __data_file = None
+
+    def __init__(self, file_path: str):
+        self.__data_file = file_path
+        self.__connect()
+
+    @property
+    def data_file(self):
+        return self.__data_file
+
+    @data_file.setter
+    def data_file(self, value):
+
+        self.__data_file = value
+        self.__connect()
+
+    def __connect(self):
+        """
+        Проверка на существование файла с данными и
+        создание его при необходимости
+        Также проверить на деградацию и возбудить исключение
+        если файл потерял актуальность в структуре данных
+        """
+        if not os.path.isfile('../course_parser/filename.json'):
+            raise FileNotFoundError("Файл filename.json отсутствует")
+        with open(self.__data_file, 'r', encoding="utf8") as file:
+            json_reader = json.load(file)
+           # print(len(json_reader))
+            if not isinstance(json_reader, list):
+                raise Exception('Файл должен содержать список')
+
+
+    def insert(self, path, data):
+        """
+        Запись данных в файл с сохранением структуры и исходных данных
+        """
+        with open(path, 'w', encoding="UTF-8") as file:
+            json.dump([e.to_dict() for e in data], file, indent=4, ensure_ascii=False, skipkeys=True, sort_keys=True)
+
+    def select(self, query: dict):  # query в данном случае словарь
+        """
+        Выбор данных из файла с применением фильтрации
+        query содержит словарь, в котором ключ это поле для
+        фильтрации, а значение это искомое значение, например:
+        {'price': 1000}, должно отфильтровать данные по полю price
+        и вернуть все строки, в которых цена 1000
+        """
+        result = []
+        with open(self.__data_file, 'r', encoding="UTF-8") as file:
+            data = json.load(file)  # считывает файл и возвразает объекты Python
+
+        if not query:
+            return data
+
+        for item in data:
+            for key, value in query.items():
+                if item.get(key) == value:
+                    result.append(item)
+
+        return result
---
+++ vacancy.py
@@ -0,0 +1,74 @@
+import datetime
+
+
+class Vacancy:
+
+    def __init__(self, name='', url='', description='', salary='', date_published=datetime.datetime.now(), **kwargs):
+        self.name = name
+        self.url = url
+        self.description = description
+        self.salary = salary
+        self.date_published = date_published
+
+    def __lt__(self, other):
+        return self.date_published < other.date_published
+
+    def __str__(self):
+        return f'Вакансия - {self.name}, заработная плата - {self.salary} \n;'
+
+    def to_dict(self):
+        return {
+            'url': self.url,
+            'name': self.name,
+            'description': self.description,
+            'salary': self.salary,
+            'date_puplished': self.date_published
+        }
+
+class HHVacancy(Vacancy):
+    """ HeadHunter Vacancy """
+
+    def __repr__(self):
+        return f"HH: {self.name}, зарплата: {self.salary} руб/мес \n;"
+
+    def __str__(self):
+        return f'HH: {self.name}, зарплата: {self.salary} руб/мес \n;'
+
+    @property
+    def max_salary(self):
+        value = self.salary.get('to', 0)
+        return 0 if value is None else value
+
+    def __gt__(self, other):
+        return self.date_published > other.date_published
+
+    @property
+    def datetime(self):
+        value = self.date_published
+        return value
+
+    #
+
+
+
+class SJVacancy(Vacancy):
+    """ SuperJob Vacancy """
+
+    def __repr__(self):
+        return f"SJ: {self.name}, зарплата: {self.salary} руб/мес \n;"
+
+    def __str__(self):
+        return f'SJ: {self.name}, зарплата: {self.salary} руб/мес \n;'
+
+    @property
+    def max_salary(self):
+        value = self.salary
+        return 0 if value is None else value
+
+    def __gt__(self, other):
+        return self.date_published > other.date_published
+
+    @property
+    def datetime(self):
+        value = self.date_published
+        return value
